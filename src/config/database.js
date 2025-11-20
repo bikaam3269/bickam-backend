4288e894a206dca86ad69ace8e092e5d30b38c3f
Mustafa Eisa <116859259+Mustafa-isa@users.noreply.github.com>
2025-11-20 03:32:03 -0800

    ...

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 export const dbConfig = {
   development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'bickam_db',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
+    username: 'freedb_bikkam_user',
+    password: 'BCbaWgWRP3@#PJZ',
+    database: 'freedb_bikaam',
+    host: 'sql.freedb.tech',
+    port: 3306,
     dialect: 'mysql',
     logging: console.log,
     dialectOptions: {
@@ -12,17 +12,15 @@ export const dbConfig = {
     }
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    username: 'freedb_bikkam_user',
+    password: 'BCbaWgWRP3@#PJZ',
+    database: 'freedb_bikaam',
+    host: 'sql.freedb.tech',
+    port: 3306,
     dialect: 'mysql',
-    logging: false,
+    logging: console.log,
     dialectOptions: {
       connectTimeout: 60000
     }
   }
 };
-
-
